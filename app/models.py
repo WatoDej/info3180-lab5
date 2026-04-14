e98624bea451dd71339ad895ffcdf4f47fcefca7
WatoDej <watsondejone5@gmail.com>
2026-04-13 23:35:18 -0500

    exercise 3 complete

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
 # Add any model classes for Flask-SQLAlchemy here
+from app import db
+from datetime import datetime
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.Text, nullable=False)
+    poster =  db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
--- app/views.py
+++ app/views.py
@@ -5,9 +5,14 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, jsonify, send_file
+from app import app, db
+from flask import render_template, request, jsonify, send_file, send_from_directory
 import os
+from flask_wtf.csrf import generate_csrf 
+from werkzeug.utils import secure_filename
+from app.forms import MovieForm
+from app.models import Movie
+
 
 
 ###
@@ -18,6 +23,35 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm()
+
+    if form.validate_on_submit():
+        poster = form.poster.data
+        filename = secure_filename(poster.filename)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        movie = Movie(
+            title=form.title.data,
+            description=form.description.data,
+            poster=filename
+        )
+
+        db.session.add(movie)
+        db.session.commit()
+
+        response_data = {
+            "message": "Movie successfully added",
+            "title": form.title.data,
+            "description": form.description.data,
+            "poster": filename
+        }
+        return jsonify(response_data), 201
+
+    else:
+        errors = form_errors(form)
+        return jsonify({"errors": errors}), 400
 
 ###
 # The functions below should be applicable to all Flask apps.
